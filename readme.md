f3ea83bd45b7433e72086c90d5dd6ff8a8abfa41
Blossom <saphashb@gmail.com>
2022-06-16 02:25:55 +0100

    rename project

--- readme.md
+++ readme.md
@@ -1,3 +1,2 @@
-## Zion API
+## Tech Trant
 
-This repo contains code to Zion's API. _Zion is a school management system._
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,21 +1,26 @@
 import User from "../models/User";
 import { Request, Response, Application } from "express";
+import bcrypt from 'bcrypt'
+
+const { PEPPER, SALT } = process.env
 
 const register = async (req: Request, res: Response) => {
   try {
+    const hashedPwd = await bcrypt.hash(req.body.password + PEPPER, Number(SALT))
     const data = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password
+      password: hashedPwd
     })
     const user = await data.save()
     res.status(200).json(user)
   } catch (error) {
+    console.log(error)
     res.status(500).json(error)
   }
 }
 
-const AuthRoute = (app:Application) => {
+const AuthRoute = (app: Application) => {
   app.post('/api/v1/register', register)
 }
 
