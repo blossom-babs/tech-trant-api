b9bdef76cab78137a0e8abe15f3295f7319c511d
Blossom <saphashb@gmail.com>
2022-06-16 01:24:22 +0100

    create post model

---
+++ src/models/Post.tsx
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		desc: {
+			type: String,
+			required: true
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		photo: {
+			type: String,
+			required: false
+		},
+		category: {
+			type: Array,
+			required: false
+		}
+	},
+	{ timestamps: true }
+);
+
+export default mongoose.model('Post', PostSchema);
