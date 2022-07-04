8754ef69bf6d9fcb8996070024d686959623371c
Blossom <saphashb@gmail.com>
2022-07-04 10:41:22 +0100

    update already created post

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -3,7 +3,7 @@ import { Post, User } from "../models";
 
 // create post
 const create = async (req: Request, res: Response) => {
-  const user = await User.findById(req.params.id)
+  const user = await User.findById(req.params.userId)
   const category = req.body.categories.split(',')
   try {
     const data = new Post({
@@ -22,12 +22,13 @@ const create = async (req: Request, res: Response) => {
 
 // delete post
 const remove = async (req: Request, res: Response) => {
-  if (req.params.id !== req.body.userId) {
+  const post = await Post.findOne({ _id: req.params.postId })
+  if (post.author !== req.body.author) {
     res.status(401).json({ Message: 'You are not authorized to perform this action' })
     return
   }
   try {
-    await Post.findOneAndDelete({ title: req.body.title })
+    await Post.findByIdAndDelete(req.params.postId)
     res.status(200).json({ Message: 'Post deleted' })
   } catch (error) {
     res.status(500).json(error)
@@ -47,8 +48,25 @@ const index = async (req: Request, res: Response) => {
 // get single post
 const post = async (req: Request, res: Response) => {
   try {
-    const post = await Post.findOne({_id: req.params.postId})
+    const post = await Post.findOne({ _id: req.params.postId })
     res.status(200).json(post)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+// update post
+const update = async (req: Request, res: Response) => {
+  const post = await Post.findOne({ _id: req.params.postId })
+  if (post.author !== req.body.author) {
+    res.status(401).json({ Message: 'You are not authorized to perform this action' })
+    return
+  }
+  try {
+    req.body.categories = req.body.categories.split(',')
+    const updatedPost = await Post.findByIdAndUpdate(req.params.postId, { $set: req.body },
+      { new: true })
+    res.status(200).json(updatedPost)
   } catch (error) {
     res.status(500).json(error)
   }
@@ -57,10 +75,11 @@ const post = async (req: Request, res: Response) => {
 
 
 const PostRoute = (app: Application) => {
-  app.post('/api/v1/post/:id', create)
-  app.delete('/api/v1/post/:id', remove)
+  app.post('/api/v1/post/:userId', create)
+  app.delete('/api/v1/post/:postId', remove)
   app.get('/api/v1/posts', index)
   app.get('/api/v1/post/:postId', post)
+  app.put('/api/v1/post/:postId', update)
 }
 
 export default PostRoute
