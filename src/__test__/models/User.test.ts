b8ae3f75a4f096120ff2ecdbbdabc0d68e297891
Blossom <saphashb@gmail.com>
2022-07-05 12:04:37 +0100

    test user model

---
+++ src/__test__/models/User.test.ts
@@ -0,0 +1,38 @@
+import User from "../../models/User";
+import { dropCollections, dropDatabase, setUp } from '../../setup/db';
+
+const userData = {
+  username: "Seal",
+  email: "seal@gmail.com",
+  password: "eal1",
+  hobby: "cookery"
+};
+beforeEach(async () => {
+  await setUp()
+
+})
+
+afterEach(async () => {
+  await dropCollections()
+  await dropDatabase()
+})
+
+
+
+describe('User model', () => {
+
+  it('create and saves user successfully', async () => {
+    const data = new User(userData)
+    const user = await data.save()
+    expect(user._id).toBeDefined()
+    expect(user.email).toBe(data.email)
+    expect(user.username).toBe(data.username)
+  })
+
+  it('field not defined in the schema should be undefined', async () => {
+    const data = new User(userData)
+    const user = await data.save()
+    expect(user._id).toBeDefined()
+    expect(user.hobby).toBeUndefined()
+  })
+})
