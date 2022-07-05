3bf8bf7c318ea17e649c35e469ea306baaac34e6
Blossom <saphashb@gmail.com>
2022-07-05 09:58:37 +0100

    create test file for routes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
+const { PEPPER, SALT } = process.env
 
 const UserSchema = new mongoose.Schema({
 	username: {
@@ -20,5 +22,25 @@ const UserSchema = new mongoose.Schema({
 		default: ''
 	}
 }, {timestamps: true});
+
+UserSchema.pre('save', function(next) {
+	var user = this;
+
+	// only hash the password if it has been modified (or is new)
+	if (!user.isModified('password')) return next();
+
+	// generate a salt
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+			if (err) return next(err);
+
+			// hash the password using our new salt
+			bcrypt.hash(user.password, salt, function(err, hash) {
+					if (err) return next(err);
+					// override the cleartext password with the hashed one
+					user.password = hash;
+					next();
+			});
+	});
+});
 
 export default mongoose.model('User', UserSchema);
