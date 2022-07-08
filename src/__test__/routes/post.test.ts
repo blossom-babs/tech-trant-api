0ea66ae951fd13e831c431df8d24bc18808f3320
Blossom <saphashb@gmail.com>
2022-07-08 12:20:24 +0100

    create the mongo uri

--- src/__test__/routes/post.test.ts
+++ src/__test__/routes/post.test.ts
@@ -1,13 +1,25 @@
 import supertest from 'supertest';
 import app from "../../app"
+import { setUp, dropCollections, dropDatabase } from '../../setup/db';
 
 describe(('Post'), () => {
+  beforeEach(async () => {
+    await setUp()
+  })
+
+  afterEach(async () => {
+    await dropCollections()
+    await dropDatabase()
+  })
+  
+  const req = supertest(app)
   describe('GET post route', () => {
     describe("given the product does not exist", () => {
       it('should return a 404', async () => {
         expect(true).toBe(true)
         const productId = 'product-123'
-        await supertest(app).get(`/api/v1/post/${productId}`)
+        const result = await req.get(`/api/v1/posts`)
+        expect(result.status).toBe(200)
       })
     })
   })
