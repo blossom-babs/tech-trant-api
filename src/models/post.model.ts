0c43e8dc8b3476c25e2b1b416139c332d1757588
Blossom <saphashb@gmail.com>
2022-07-23 21:17:56 +0100

    export default post model

--- src/models/post.model.ts
+++ src/models/post.model.ts
@@ -27,4 +27,5 @@ const PostSchema = new mongoose.Schema(
 	{ timestamps: true }
 );
 
-export default mongoose.model('Post', PostSchema);
+const PostModel =  mongoose.model('Post', PostSchema);
+export default PostModel
