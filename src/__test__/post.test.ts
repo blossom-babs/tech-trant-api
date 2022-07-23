152b05878e9995d4688a1f68e12f86bbf4400d1a
Blossom <saphashb@gmail.com>
2022-07-23 22:00:47 +0100

    create mongo memory in server

--- src/__test__/post.test.ts
+++ src/__test__/post.test.ts
@@ -1,11 +1,24 @@
+import  mongoose  from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import supertest from 'supertest'
 import app from '../app'
 
 describe('post', () => {
+
+  beforeAll(async () => {
+    const mongoServer = await MongoMemoryServer.create()
+    await mongoose.connect(mongoServer.getUri())
+  })
+
+  afterAll(async () => {
+    await mongoose.disconnect()
+    await mongoose.connection.close()
+  })
+
   describe ('get post', () => {
     describe('given the product does not exist', () => {
       it('should return a 404', async () => {
-        const postId = 'post-123'
+        const postId = '62c2bc3b3fcaa7bddbf298d'
         await supertest(app).get(`/api/v1/post/${postId}`).expect(500)
       })
     })
