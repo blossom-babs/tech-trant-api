76c00fbdd37d350dc51b0f7db0eb7518503e3f1d
Blossom <saphashb@gmail.com>
2022-07-24 01:11:53 +0100

    test posts

--- note.md
+++ note.md
@@ -4,3 +4,5 @@ TODO:
 2. Authentication with jwt
 3. On login and sign-up, the username and email should be saved and checked as all lowercase
 4. Test update user
+5. curious how categories are going to be saved
+6. Errors are not handled properly --> 
--- src/__test__/routes/post.routes.test.ts
+++ src/__test__/routes/post.routes.test.ts
@@ -1,21 +1,40 @@
-import  mongoose  from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
+import { dropCollections, dropDatabase, setUp } from '../setup/db';
 import supertest from 'supertest'
 import app from '../../app'
+import mongoose from 'mongoose';
 
-describe('post', () => {
+describe('POST', () => {
 
   beforeAll(async () => {
-    const mongoServer = await MongoMemoryServer.create()
-    await mongoose.connect(mongoServer.getUri())
+    await setUp()
+
   })
 
   afterAll(async () => {
-    await mongoose.disconnect()
-    await mongoose.connection.close()
+    await dropCollections()
+    await dropDatabase()
   })
 
-  describe ('get post', () => {
+  let userId = new mongoose.Types.ObjectId().toString();
+
+  const postPayload = {
+    title: 'Post one',
+    description: 'First Entry',
+    author: 'Blossom',
+    photo: '',
+    userId,
+    categories: 'coding, programming'
+  }
+
+  describe('Given a NULL ID', () => {
+    it('should not be able to create a post', async () => {
+      const response = await supertest(app).post(`/api/v1/posts`).send(postPayload)
+      expect(response.body).toEqual({})
+      expect(response.status).toBe(500)
+    })
+  })
+
+  describe('GET post', () => {
     describe('given the product does not exist', () => {
       it('should return a 500', async () => {
         const postId = '62c2bc3b3fcaa7bddbf298d'
@@ -23,7 +42,7 @@ describe('post', () => {
       })
     })
 
-    describe.only('given the product does not exist', () => {
+    describe('given the product Exists', () => {
       it('should return a 200', async () => {
         const postId = '62c2bc3b3fcaa7bddbf2928d'
         await supertest(app).get(`/api/v1/post/${postId}`).expect(200)
